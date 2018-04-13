3b7b127cc393ff6db72c8f09b6c992b7d221fdec
alexmoitta <alexandre.dos.santos@gmail.com>
2018-04-13 13:40:47 -0300

    I created plotSinCos notebook.

---
+++ octave/iterativeVariableForReplacement.m
@@ -0,0 +1,7 @@
+t = [0:0.01:0.98] % it starts with 0, increments 0.01 until 0.98
+
+t = [0:1:10] 
+
+t = [10:-1:0]
+
+t = [0:-1:10] % it shall be empty
---
+++ octave/plotSinCos.m
@@ -0,0 +1,33 @@
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+t = [0:0.01:0.98]
+
+y1 = sin(2*pi*4*t)
+
+plot(t,y1)
+
+hold on; % it will use the same frame to plot the following chart
+
+y2 = cos(2*pi*4*t)
+
+plot(t,y2)
+
+% ============================================================
+
+end
