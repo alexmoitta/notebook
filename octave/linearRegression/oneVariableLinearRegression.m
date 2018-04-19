5b3d2a88c756257fc9eb5db647f2225eb6e15a18
alexmoitta <alexandre.dos.santos@gmail.com>
2018-04-19 08:05:58 -0300

    I fixed oneVariableLinerRegression

--- octave/linearRegression/oneVariableLinearRegression.m
+++ octave/linearRegression/oneVariableLinearRegression.m
@@ -6,6 +6,7 @@ clear ; close all; clc
 x = [0;1;2;3]
 
 y = [4;7;7;8]
+%y = [0;2;4;6]
 
 
 
@@ -59,45 +60,45 @@ hold off % don't overlay any more plots on this figure
 % Use vector multiplication to predict
 % Predict values for population x = 10 
 predict1 = [1, 10] *theta;
-fprintf('For x = 10, we predict %f\n - it should be 16.70',...predict1);
+fprintf('For x = 10, we predict %f\n - it should be 16.70',predict1);
 
 
-pause;
+% pause;
 
 
 
 
-% %% ============= Part 4: Visualizing J(theta_0, theta_1) =============
-% fprintf('Visualizing J(theta_0, theta_1) ...\n')
+%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
+fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
-% % Grid over which we will calculate J
-% theta0_vals = linspace(-10, 10, 100);
-% theta1_vals = linspace(-1, 4, 100);
+% Grid over which we will calculate J
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
 
-% % initialize J_vals to a matrix of 0's
-% J_vals = zeros(length(theta0_vals), length(theta1_vals));
+% initialize J_vals to a matrix of 0's
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
-% % Fill out J_vals
-% for i = 1:length(theta0_vals)
-%     for j = 1:length(theta1_vals)
-% 	  t = [theta0_vals(i); theta1_vals(j)];
-% 	  J_vals(i,j) = computeCost(X, y, t);
-%     end
-% end
+% Fill out J_vals
+for i = 1:length(theta0_vals)
+   for j = 1:length(theta1_vals)
+  t = [theta0_vals(i); theta1_vals(j)];
+  J_vals(i,j) = computeCost(X, y, t);
+   end
+end
 
 
-% % Because of the way meshgrids work in the surf command, we need to
-% % transpose J_vals before calling surf, or else the axes will be flipped
-% J_vals = J_vals';
-% % Surface plot
-% figure;
-% surf(theta0_vals, theta1_vals, J_vals)
-% xlabel('\theta_0'); ylabel('\theta_1');
+% Because of the way meshgrids work in the surf command, we need to
+% transpose J_vals before calling surf, or else the axes will be flipped
+J_vals = J_vals';
+% Surface plot
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1');
 
-% % Contour plot
-% figure;
-% % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-% contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-% xlabel('\theta_0'); ylabel('\theta_1');
-% hold on;
-% plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+% Contour plot
+figure;
+% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('\theta_0'); ylabel('\theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
